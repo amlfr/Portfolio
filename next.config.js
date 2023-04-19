3f6a9c0615cfbd1aada8ce7cd3b8b9d59161f090
amlfr <amartinez.lalenec@gmail.com>
2023-04-19 18:58:17 +0200

    adding a CSP

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
+const securityHeaders = [
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+    },
+    {
+        key: "Content-Security-Policy",
+        value: "default-src 'self;",
+    },
+];
+
 const nextConfig = {
+    async Headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: securityHeaders,
+            },
+        ];
+    },
+
     reactStrictMode: true,
 
     //Sets up sass to use variables in every files
